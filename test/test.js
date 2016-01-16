ea9dbc40183f2df9687cadc38c4ddd8e111630cb
Planeshifter <pburckhardt@outlook.com>
2016-01-15 23:58:51 -0500

    Rename tape variable

--- test/test.js
+++ test/test.js
@@ -2,7 +2,7 @@
 
 // MODULES //
 
-var test = require( 'tape' );
+var tape = require( 'tape' );
 var incrspace = require( 'compute-incrspace' );
 var abs = require( 'math-abs' );
 var gamma = require( './../lib' );
@@ -18,12 +18,12 @@ var expected2 = require( './fixtures/expected2.json' );
 
 // TESTS //
 
-test( 'main export is a function', function test( t ) {
+tape( 'main export is a function', function test( t ) {
 	t.ok( typeof gamma === 'function', 'main export is a function' );
 	t.end();
 });
 
-test( 'if provided a negative integer, the function returns `NaN`', function test( t ) {
+tape( 'if provided a negative integer, the function returns `NaN`', function test( t ) {
 	var values = incrspace( -1, -1000, -1 );
 	var v;
 	var i;
@@ -35,31 +35,31 @@ test( 'if provided a negative integer, the function returns `NaN`', function tes
 	t.end();
 });
 
-test( 'if provided negative infinity, the function returns `NaN`', function test( t ) {
+tape( 'if provided negative infinity, the function returns `NaN`', function test( t ) {
 	var v = gamma( Number.NEGATIVE_INFINITY );
 	t.ok( v !== v, 'returns NaN when provided negative infinity' );
 	t.end();
 });
 
-test( 'if provided `NaN`, the function returns `NaN`', function test( t ) {
+tape( 'if provided `NaN`, the function returns `NaN`', function test( t ) {
 	var v = gamma( NaN );
 	t.ok( v !== v, 'returns NaN when provided a NaN' );
 	t.end();
 });
 
-test( 'if provided `-0`, the function returns negative infinity', function test( t ) {
+tape( 'if provided `-0`, the function returns negative infinity', function test( t ) {
 	var v = gamma( -0 );
 	t.equal( v, Number.NEGATIVE_INFINITY, 'returns -infinity' );
 	t.end();
 });
 
-test( 'if provided `+0`, the function returns positive infinity', function test( t ) {
+tape( 'if provided `+0`, the function returns positive infinity', function test( t ) {
 	var v = gamma( 0 );
 	t.equal( v, Number.POSITIVE_INFINITY, 'returns +infinity' );
 	t.end();
 });
 
-test( 'if `x > 171.6144...`, the function returns positive infinity', function test( t ) {
+tape( 'if `x > 171.6144...`, the function returns positive infinity', function test( t ) {
 	var values = incrspace( 172, 1000, 10.1234 );
 	var v;
 	var i;
@@ -71,7 +71,7 @@ test( 'if `x > 171.6144...`, the function returns positive infinity', function t
 	t.end();
 });
 
-test( 'if `x < -170.56749...`, the function returns positive infinity', function test( t ) {
+tape( 'if `x < -170.56749...`, the function returns positive infinity', function test( t ) {
 	var values = incrspace( -170.57, -1000, -10.1234 );
 	var v;
 	var i;
@@ -83,7 +83,7 @@ test( 'if `x < -170.56749...`, the function returns positive infinity', function
 	t.end();
 });
 
-test( 'the function evaluates the gamma function (positive integers)', function test( t ) {
+tape( 'the function evaluates the gamma function (positive integers)', function test( t ) {
 	var delta;
 	var tol;
 	var v;
@@ -98,7 +98,7 @@ test( 'the function evaluates the gamma function (positive integers)', function 
 	t.end();
 });
 
-test( 'the function evaluates the gamma function (decimal values)', function test( t ) {
+tape( 'the function evaluates the gamma function (decimal values)', function test( t ) {
 	var delta;
 	var tol;
 	var v;
@@ -113,14 +113,14 @@ test( 'the function evaluates the gamma function (decimal values)', function tes
 	t.end();
 });
 
-test( 'if provided a positive integer, the function returns the factorial of (n-1)', function test( t ) {
+tape( 'if provided a positive integer, the function returns the factorial of (n-1)', function test( t ) {
 	t.equal( gamma( 4 ), 6, 'returns 6' );
 	t.equal( gamma( 5 ), 24, 'returns 24' );
 	t.equal( gamma( 6 ), 120, 'returns 120' );
 	t.end();
 });
 
-test( 'the function uses a small value approximation near left boundaries (abs(x)<33)', function test( t ) {
+tape( 'the function uses a small value approximation near left boundaries (abs(x)<33)', function test( t ) {
 	var expected;
 	var delta;
 	var tol;
@@ -140,7 +140,7 @@ test( 'the function uses a small value approximation near left boundaries (abs(x
 	t.end();
 });
 
-test( 'the function uses a small value approximation near right boundaries (abs(x)<33)', function test( t ) {
+tape( 'the function uses a small value approximation near right boundaries (abs(x)<33)', function test( t ) {
 	var expected;
 	var delta;
 	var tol;
